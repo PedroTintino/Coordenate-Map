78e8b0d08d19e0b834780e21bbac2962b2f86ebf
Kaworu desu <pedro.arcanjo2002@gmail.com>
2023-03-30 23:57:30 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ L.marker([-23.965069, -46.375210])
 
     //São Vicente
 L.marker([-23.965519, -46.386695])
-.bindPopup('<h3>São Vicente: </h3><p>Total de eleitores: </p>')
+.bindPopup('<h4>São Vicente: </h4><p>Total de eleitores: </p>')
 .openPopup(),
 
     //Vila Margarida
@@ -215,11 +215,11 @@ marcadorBairros.addEventListener('change', function(){
 
 var marcadoresDeZona = [
 L.marker([-23.96894056396401, -46.386882575037404])
-.bindPopup('<h3>Zona 1(117)</h3>')
+.bindPopup('<h4>Zona 1(117)</h4>')
 .openPopup(),
 
 L.marker([-23.964486350412447, -46.382906916377635])
-.bindPopup('<h3>Zona 2(340)</h3>')
+.bindPopup('<h4>Zona 2(340)</h4>')
 .openPopup()
 ];
 var layerGroupTwo = L.layerGroup(marcadoresDeZona);
@@ -251,147 +251,152 @@ marcadorZonas.addEventListener('change', function(){
 
 //Colégios
 
+//Zona 117
+
 var marcadoresDeEscolas = [
     L.marker([-23.966594352758225, -46.37687635740972])
-    .bindPopup('<h3> Colégio Ateneu São Vicente 1</h3>')
+    .bindPopup('<h4> Colégio Ateneu São Vicente 1</h4>')
     .openPopup(),
     
     L.marker([-23.964676452759452, -46.37970623172126])
-    .bindPopup('<h3> COLÉGIO ÊXITO</h3>')
+    .bindPopup('<h4> COLÉGIO ÊXITO</h4>')
     .openPopup(),
     
     L.marker([-23.9684120550308, -46.38826546055712])
-    .bindPopup('<h3>COLÉGIO ITA - UNIDADE I </h3>')
+    .bindPopup('<h4>COLÉGIO ITA - UNIDADE I </h4>')
     .openPopup(),
     
     L.marker([-23.965994965149047, -46.379903902885275])
-    .bindPopup('<h3>COLÉGIO ITA UNIDADE II</h3>')
+    .bindPopup('<h4>COLÉGIO ITA UNIDADE II</h4>')
     .openPopup(),
     
     L.marker([-23.954661384482478, -46.373505674049674])
-    .bindPopup('<h3>COLÉGIO NASCIMENTO</h3>')
+    .bindPopup('<h4>COLÉGIO NASCIMENTO</h4>')
     .openPopup(),
     
     L.marker([-23.968321643679356, -46.35676972634301])
-    .bindPopup('<h3>COLÉGIO NOTRE DAME - UNIDADE I</h3>')
+    .bindPopup('<h4>COLÉGIO NOTRE DAME - UNIDADE I</h4>')
     .openPopup(),
    
     L.marker([-23.96958739428028, -46.369652185552155])
-    .bindPopup('<h3>COLÉGIO NOTRE DAME - UNIDADE II</h3>')
+    .bindPopup('<h4>COLÉGIO NOTRE DAME - UNIDADE II</h4>')
     .openPopup(),
    
     L.marker([-23.96296325361791, -46.39435665487066])
-    .bindPopup('<h3>COLÉGIO SANTA ROSA</h3>')
+    .bindPopup('<h4>COLÉGIO SANTA ROSA</h4>')
     .openPopup(),
    
     L.marker([-23.97289110531335, -46.39869726281325])
-    .bindPopup('<h3>COLÉGIO NOTRE DAME - UNIDADE II</h3>')
+    .bindPopup('<h4>COLÉGIO NOTRE DAME - UNIDADE II</h4>')
     .openPopup(),
    
     L.marker([-23.96958739428028, -46.369652185552155])
-    .bindPopup('<h3>EE.PASTOR JOAQUIM LOPES LEÃO</h3>')
+    .bindPopup('<h4>EE.PASTOR JOAQUIM LOPES LEÃO</h4>')
     .openPopup(),
     
     L.marker([-23.96543434254604, -46.39400120089615])
-    .bindPopup('<h3>EE.PROF.LEOPOLDO JOSÉ DE SANT ANNA</h3>')
+    .bindPopup('<h4>EE.PROF.LEOPOLDO JOSÉ DE SANT ANNA</h4>')
     .openPopup(),
     
     L.marker([-23.96579198160695, -46.40122590089597])
-    .bindPopup('<h3>EE. MARGARIDA PINHO RODRIGUES</h3>')
+    .bindPopup('<h4>EE. MARGARIDA PINHO RODRIGUES</h4>')
     .openPopup(),
     
     L.marker([-23.96958739428028, -46.369652185552155])
-    .bindPopup('<h3>EE. ANTONIO LUIZ BARREIROS</h3>')
+    .bindPopup('<h4>EE. ANTONIO LUIZ BARREIROS</h4>')
     .openPopup(),
     
     L.marker([-23.968904935415424, -46.38350728555252])
-    .bindPopup('<h3>EE MARTIM AFONSO</h3>')
+    .bindPopup('<h4>EE MARTIM AFONSO</h4>')
     .openPopup(),
     
     L.marker([-23.96798791842808, -46.40925803952632])
-    .bindPopup('<h3>EE PROF LUCIO MARTINS RODRIGUES</h3>')
+    .bindPopup('<h4>EE PROF LUCIO MARTINS RODRIGUES</h4>')
     .openPopup(),
     
     L.marker([-23.955100590012613, -46.369656985558265])
-    .bindPopup('<h3>EE.PROFA.ZULMIRA DE ALMEIDA LAMBERT</h3>')
+    .bindPopup('<h4>EE.PROFA.ZULMIRA DE ALMEIDA LAMBERT</h4>')
     .openPopup(),
     
     L.marker([-23.963958882755264, -46.370317731855025])
-    .bindPopup('<h3>EMEF LIONS CLUBE</h3>')
+    .bindPopup('<h4>EMEF LIONS CLUBE</h4>')
     .openPopup(),
     
     L.marker([-23.96942359409514, -46.40477317021017])
-    .bindPopup('<h3>EMEF. ERCÍLIA NOGUEIRA COBRA</h3>')
+    .bindPopup('<h4>EMEF. ERCÍLIA NOGUEIRA COBRA</h4>')
     .openPopup(),
     
     L.marker([-23.962479918706517, -46.36940055487092])
-    .bindPopup('<h3>EMEF. PREFEITO ANTONIO FERNANDO DOS REIS</h3>')
+    .bindPopup('<h4>EMEF. PREFEITO ANTONIO FERNANDO DOS REIS</h4>')
     .openPopup(),
     
     L.marker([-23.96698471573899, -46.40180667016806])
-    .bindPopup('<h3>EMEF. PREFEITO LUIZ BENEDITINO FERREIRA</h3>')
+    .bindPopup('<h4>EMEF. PREFEITO LUIZ BENEDITINO FERREIRA</h4>')
     .openPopup(),
     
     L.marker([-23.961177249401793, -46.37806178555569])
-    .bindPopup('<h3>EMEF PROF CONSTANTE LUCIANO CLEMENTE HOULMONT</h3>')
+    .bindPopup('<h4>EMEF PROF CONSTANTE LUCIANO CLEMENTE HOULMONT</h4>')
     .openPopup(),
     
     L.marker([-23.961468867683422, -46.4072692548713])
-    .bindPopup('<h3>EMEF. PROFA. LAURA FILGUEIRAS</h3>')
+    .bindPopup('<h4>EMEF. PROFA. LAURA FILGUEIRAS</h4>')
     .openPopup(),
     
     L.marker([-23.971402501906077, -46.39135390201291])
-    .bindPopup('<h3>EMEF RAQUEL DE CASTRO FERREIRA</h3>')
+    .bindPopup('<h4>EMEF RAQUEL DE CASTRO FERREIRA</h4>')
     .openPopup(),
     
     L.marker([-23.971411900373226, -46.39278112418262])
-    .bindPopup('<h3>EMEF. REPÚBLICA DE PORTUGAL</h3>')
+    .bindPopup('<h4>EMEF. REPÚBLICA DE PORTUGAL</h4>')
     .openPopup(),
     
     L.marker([-23.98766253383847, -46.392845770202484])
-    .bindPopup('<h3>EMEI. MARIA MATHILDE DE SANTANA</h3>')
+    .bindPopup('<h4>EMEI. MARIA MATHILDE DE SANTANA</h4>')
     .openPopup(),
     
     L.marker([-23.961421967612488, -46.36942867021354])
-    .bindPopup('<h3>EMEI. PROF. EDMUNDO CAPELLARI</h3>')
+    .bindPopup('<h4>EMEI. PROF. EDMUNDO CAPELLARI</h4>')
     .openPopup(),
     
     L.marker([-23.961515413961056, -46.40628513059977])
-    .bindPopup('<h3>EMEI PROFA. EULINA TRINDADE</h3>')
+    .bindPopup('<h4>EMEI PROFA. EULINA TRINDADE</h4>')
     .openPopup(),
     
     L.marker([-23.967511184482785, -46.40881030360994])
-    .bindPopup('<h3>EMEI. PROVÍNCIA DE OKINAWA</h3>')
+    .bindPopup('<h4>EMEI. PROVÍNCIA DE OKINAWA</h4>')
     .openPopup(),
     
     L.marker([-23.969425670061806, -46.39189007477478])
-    .bindPopup('<h3>Escola Henrique Oswald</h3>')
+    .bindPopup('<h4>Escola Henrique Oswald</h4>')
     .openPopup(),
     
     L.marker([-23.965147072691458, -46.3835947036099])
-    .bindPopup('<h3>ETEC Dra. Ruth Cardoso</h3>')
+    .bindPopup('<h4>ETEC Dra. Ruth Cardoso</h4>')
     .openPopup(),
     
     L.marker([-23.967624358296337, -46.37848953244499])
-    .bindPopup('<h3>ESCOLA TÉCNICA FORTEC</h3>')
+    .bindPopup('<h4>ESCOLA TÉCNICA FORTEC</h4>')
     .openPopup(),
     
     L.marker([-23.96966555579966, -46.369747918949905])
-    .bindPopup('<h3>Escola Verde</h3>')
+    .bindPopup('<h4>Escola Verde</h4>')
     .openPopup(),
     
     L.marker([-23.964206164476167, -46.39129796127999])
-    .bindPopup('<h3>FACULDADE UNIBR	</h3>')
+    .bindPopup('<h4>FACULDADE UNIBR	</h4>')
     .openPopup(),
     
     L.marker([-23.969744480899845, -46.37301317477472])
-    .bindPopup('<h3>OBJETIVO - ASSOCIAÇÃO DE PESQUISA E ENSINO DO LITORAL</h3>')
+    .bindPopup('<h4>OBJETIVO - ASSOCIAÇÃO DE PESQUISA E ENSINO DO LITORAL</h4>')
     .openPopup(),
     
     L.marker([-23.965355167329186, -46.38182975870591])
-    .bindPopup('<h3>Colégio Batista da Primeira</h3>')
+    .bindPopup('<h4>Colégio Batista da Primeira</h4>')
     .openPopup()
-    ];
+    
+  //Zona 340
+  
+  ];
     var layerGroupThree = L.layerGroup(marcadoresDeEscolas);
     var allLayersThree = L.layerGroup([camadaMapa, layerGroupThree]);
     allLayersThree.addTo(map);
@@ -421,6 +426,91 @@ marcadorEscolas.addEventListener('change', function(){
 function avisoAdicionarinformacao(){
     alert('Para adiconar informações, clique sobre o marcador que deseja utilizar.')
 };
+
+//Padrão de objeto para entrada de dados
+
+const padraoEntradaDeDados = {
+  partido:'',
+  candidato:'',
+  votos:''
+};
+
+//Formulário de previsão
+
+const mostraFormularioBtn = document.getElementById('mostrar-form');
+const formulario = document.querySelector('.new-data-form');
+
+
+mostraFormularioBtn.addEventListener('click', function(){
+  formulario.style.display = 'block';
+});
+
+const cancelBtn = document.getElementById('cancel-btn');
+cancelBtn.addEventListener('click', function(){
+  formulario.style.display = 'none';
+});
+
+const saveBtn = document.getElementById('send-btn');
+saveBtn.addEventListener('click', function(e){
+  e.preventDefault();
+  
+})
+
+//Captação dos values dos inputs
+
+const inputCandidatos= document.getElementById('input-candidato');
+   
+
+const inputPartidos= document.getElementById('input-partido');
+
+const inputVotos= document.getElementById('input-votos');
+
+let markerSelecionado = null; // variável para armazenar o marker selecionado
+
+// Seletor de markers
+marcadoresDeZona.forEach(function(marker) {
+  marker.on('click', function() {
+    markerSelecionado = marker;
+    console.log('Marker selecionado:', marker);
+  });
+});
+
+marcadoresDeBairro.forEach(function(marker) {
+  marker.on('click', function() {
+    markerSelecionado = marker;
+    console.log('Marker selecionado:', marker);
+  });
+});
+
+marcadoresDeEscolas.forEach(function(marker) {
+  marker.on('click', function() {
+    markerSelecionado = marker;
+    console.log('Marker selecionado:', marker);
+  });
+});
+// Var para adicionar descrição 
+let textoPopup = '';
+
+function atualizarDescricao() {
+  
+  const inputPartidos= document.getElementById('input-partido');
+  const partido = inputPartidos.value;
+  const inputVotos= document.getElementById('input-votos');
+  const votos = inputVotos.value;
+  const inputCandidatos= document.getElementById('input-candidato');
+  const candidato = inputCandidatos.value;
+
+ 
+  textoPopup += '<br>' + 'Candidato: '+ '<br>' + candidato + '<br>';
+  textoPopup += '<br>' + 'Partido: '+ '<br>'+ partido + '<br>';
+  textoPopup += '<br>'  + 'Votos: '+ '<br>' + votos;
+  
+
+  // atualizar o conteúdo do popup
+  markerSelecionado.setPopupContent(textoPopup);
+ 
+}
+
 
 
 
